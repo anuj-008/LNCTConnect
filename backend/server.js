c5e96dea591c9005e47a022d5389c14065c12b8d
Anuj Singh <anujsingh2708@gmail.com>
2026-06-04 12:03:56 +0530

    Create backend structure and user routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,4 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("LNCTConnect API Running");
-});
+const app = require("./src/app");
 
 const PORT = 5000;
 
